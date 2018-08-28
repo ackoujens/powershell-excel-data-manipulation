d3cf5ff7f1c9fa2af45cfe6e35d2713e152f7582
Ackou Jens <ackoujens@gmail.com>
2018-08-28 09:25:53 +0200

    Reset vars and concatenate subroutines

--- main.ps1
+++ main.ps1
@@ -2,12 +2,10 @@
  # New Employees Prep Generator
  #>
 
-# Libraries
-. .\includes\word.ps1
-. .\includes\excel.ps1
+# Reset vars
+# TODO: Include in reset procedure
+Remove-Variable * -ErrorAction SilentlyContinue; Remove-Module *; $error.Clear(); Clear-Host;
 
-# Gather Employee numbers from Excel file
-# Tabs in Excel file are divided by meeting date
-function getEmployeeNumber([int]$index) {
-    
-}
+# Subroutines
+. compile-employee-data.ps1
+. compile-device-documents.ps1
